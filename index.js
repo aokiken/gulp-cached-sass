69d3a3568e7d919b36c030d87d297d479fbc7b2e
Ken Aoki <aokiken@me.com>
2019-02-26 08:54:17 +0900

    差分ビルド時に@importされているscssファイルを検出し、更新対象とさせるロジックを追加
    それに伴い、依存するライブラリをinstall

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var cached = require('gulp-cached');
+'use strict';
+const {parseDir} = require('sass-graph');
+const gulpForEach = require('gulp-foreach');
+const {src} = require('vinyl-fs');
+const PluginError = require('plugin-error');
 
-module.exports = cached;
+const PLUGIN_NAME = 'gulp-cached-sass';
+
+const plugin = function (baseDir) {
+    if (!baseDir) {
+        throw new PluginError(PLUGIN_NAME, 'Missing baseDir!');
+    }
+
+    const graph = parseDir(baseDir);
+    const getUpdateFile = (currentStream, file) => {
+        let files = [file.path];
+        const addParent = (childPath) =>
+            graph.visitAncestors(childPath, (parent) => {
+                if (!files.includes(parent)) {
+                    files.push(parent);
+                }
+                return addParent(parent);
+            });
+        addParent(file.path);
+        return src(files, {
+            base: baseDir
+        });
+    };
+    return gulpForEach(getUpdateFile);
+};
+
+module.exports = plugin;
